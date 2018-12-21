675e08a16977345777c2a1808940a5535c516033
Renato Carauta Ribeiro <rcarauta@unb.br>
2018-12-21 09:31:10 -0200

    Gerando certificado e lendo alguns nomes, mas ainda não lendo a chave privada

--- src/main/java/com/example/demo/certification/SingerCertificate.java
+++ src/main/java/com/example/demo/certification/SingerCertificate.java
@@ -1,5 +1,6 @@
 package com.example.demo.certification;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,6 +14,7 @@ import java.security.Signature;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 
 import org.demoiselle.signer.core.CertificateManager;
@@ -23,6 +25,9 @@ import org.demoiselle.signer.policy.impl.cades.factory.PKCS7Factory;
 import org.demoiselle.signer.policy.impl.cades.pkcs7.impl.CAdESSigner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.file.Cert;
@@ -41,7 +46,7 @@ public class SingerCertificate {
 		Cert cert = cm.load(Cert.class);	
 		BasicCertificate bc = new BasicCertificate(inputStreamFile);
 
-		System.out.println(bc.getName());
+		System.out.println(bc.getNome());
 		System.out.println(bc.getEmail());
 		System.out.println(bc.getSerialNumber());
 		
@@ -64,18 +69,18 @@ public class SingerCertificate {
 	
 	}
 	
-	@GetMapping("/certificate")
-	public byte[] assinaturaCadesDetached(@PathVariable("path")String pathFileTxt) throws Exception {
+	@RequestMapping(value = "/certificate", params= {"path"}, method = RequestMethod.GET)
+	public byte[] assinaturaCadesDetached(@RequestParam("path")String pathFileTxt) throws Exception {
 		byte[] content = readContent(pathFileTxt);
 		X509Certificate[] certificates = new X509Certificate[1];
-		CAdESSigner singer = (CAdESSigner) PKCS7Factory.getInstance().factoryDefault();
-		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(readContent("/home/rcarauta/desenvolvimento/certificado/server.key"));
+		CAdESSigner singer = (CAdESSigner) new PKCS7Factory().factoryDefault();
+		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(readContent("/home/rcarauta/desenvolvimento/certificado/ca.crt"));
 		
 		certificates[0] = getCertificate();
 		singer.setCertificates(certificates);
 		
 		KeyFactory factory = KeyFactory.getInstance("RSA");
-		PrivateKey privKey = factory.generatePrivate(spec);
+		RSAPrivateKey privKey = (RSAPrivateKey) factory.generatePrivate(spec);
 		
 		singer.setPrivateKey(privKey);
 		
@@ -84,20 +89,27 @@ public class SingerCertificate {
 	}
 	
 	private X509Certificate getCertificate() throws IOException, Exception {
-		File certFile = new File("/home/rcarauta/desenvolvimento/certificado/ca.key");
-		InputStream inputStreamFile = new FileInputStream(certFile);	
+		String pathFile = "/home/rcarauta/desenvolvimento/certificado/ca.crt";
+		File certFile = new File(pathFile);
+		InputStream inputStreamFile = new FileInputStream(certFile);
+		ByteArrayInputStream bytes = new ByteArrayInputStream(readContent(pathFile));
 		BasicCertificate bc = new BasicCertificate(inputStreamFile);
+		
 
-		System.out.println(bc.getName());
+		System.out.println(bc.getNome());
 		System.out.println(bc.getEmail());
 		System.out.println(bc.getSerialNumber());
 		
-		X509Certificate x509Certificate =(X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(inputStreamFile);
+		CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+				
+		X509Certificate	x509Certificate = (X509Certificate) certFactory.generateCertificate(bytes);
+				
 		CertificateExtra ce = new CertificateExtra(x509Certificate);
 		
-		System.out.println(ce.getOID_2_16_76_1_3_1().getCPF());
-		System.out.println(ce.getOID_2_16_76_1_3_3().getCNPJ());
-		System.out.println(ce.getOID_2_16_76_1_3_2().getName());
+		System.out.println(ce.getEmail());
+		System.out.println(ce.getOID_2_16_76_1_3_1());
+		System.out.println(ce.getOID_2_16_76_1_3_2());
+		System.out.println(ce.getOID_2_16_76_1_3_3());
 		
 		return x509Certificate;
 	}
